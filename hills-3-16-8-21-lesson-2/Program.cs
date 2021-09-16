7885fc7c6dbc732d0b6588d11efabcf1b2aeb515
Skyler Turner <skyler@clicksminuteper.net>
2021-09-16 14:45:45 +0100

    Initial commit, add circle circumference calculator

---
+++ hills-3-16-8-21-lesson-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hills_3_16_8_21_lesson_2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Program to calculate the circumference of a circle");
+            Console.Write("Enter circle radius: ");
+            
+            double radius;
+            while (!double.TryParse(Console.ReadLine(), out radius))
+            {
+                Console.Write("That wasn't a number, please try again: ");
+            }
+
+            var circumference = radius * Math.Tau; // (The formula for the circumference of a circle is the radius times tau (tau is equal to 2 pi))
+
+            Console.WriteLine($"The circumference of the circle is {Math.Round(circumference, 2)}\n" +
+                              $"Press enter to exit the program");
+            Console.ReadLine();
+        }
+    }
+}
